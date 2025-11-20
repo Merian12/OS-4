c023292d724173700d301a1437658b1e614d352a
Raaschou <mraaschou@gmail.com>
2025-11-20 12:07:18 +0100

    Add/Update bash scripts and csv files (/w results)

--- bashtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 1 5);
-do
-    ./search shakespeare-full.txt "Something is rotten in the state of Denmark." $i 8 results.csv
-done
---
+++ data8-bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in $(seq 1 20);
+do
+    ./search shakespeare-full.txt "Something is rotten in the state of Denmark." $i 8 data8.csv
+done
